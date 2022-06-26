2d3b0fe996a423c3813918df078d76ce20b84ee9
Malik. A <54292102+Placeholder30@users.noreply.github.com>
2022-06-26 01:14:42 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
         <p>
           A Full-stack Engineer that is passionate about Improving
           people&rsquo;s lives with code. When I am not writing JavaScript with
-          my favorite framework <span>{"React"}</span>, you&rsquo;ll find me on
+          my favorite framework <span>{"Express"}</span>, you&rsquo;ll find me on
           twitter talking about why Batman should be your favorite superhero.
         </p>
         <div className={styles.hitme}>
